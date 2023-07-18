fe7224dd2975cf54d6084387c4a0ba4e79630363
matheus6p <mhpaes97@gmail.com>
2023-07-18 18:19:53 -0300

    color themes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#fff',
+  'yellow-dark': '#C47F17',
+  'yellow': '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+  
+  'purple-dark': '#4B2995',
+  'purple': '#8047F8',
+  'purple-light': '#EBE5F9',
+
+  'gray-100':'#FAFAFA',
+  'gray-200':'#F3F2F2',
+  'gray-300':'#EDEDED',
+  'gray-400':'#E6E5E5',
+  'gray-500':'#D7D5D5',
+  'gray-600':'#8D8686',
+  'gray-700':'#574F4D',
+  'gray-800':'#403937',
+  'gray-900':'#272221',
+}
